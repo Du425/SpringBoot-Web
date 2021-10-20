ed4f5a3638eb7644097792ae649f34fcb4b4a166
7399 <1910674037@qq.com>
2021-10-20 20:22:07 +0800

    1

--- src/main/java/com/du/contoller/EmployeeController.java
+++ src/main/java/com/du/contoller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.du.contoller;
 
+import com.du.config.pojo.Department;
 import com.du.config.pojo.Employee;
+import com.du.dao.DepartmenrDao;
 import com.du.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,12 +19,28 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmenrDao departmenrDao;
+
 
     @RequestMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
         return "emp/list";
+    }
+    @GetMapping("/emp")
+    public String toAddpage(Model model){
+        //查出所有部门的信息
+        Collection<Department> department = departmenrDao.getDepartment();
+        model.addAttribute("department",department);
+        return "emp/add";
+    }
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("save=>"+employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 
 }
